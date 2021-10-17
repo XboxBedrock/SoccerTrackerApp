d78c495b2e07d4d9dddcc8be69562c9f9017ca98
XboxBedrock <68715625+XboxBedrock@users.noreply.github.com>
2021-10-16 23:09:37 -0700

    e

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+}
+
+app.allowRendererProcessReuse = false
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function() {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
---
+++ noise-filter.js
@@ -0,0 +1,25 @@
+const NP_ACCEL = 0.119;
+const NPS_ACCEL = 0.0898;
+const GAIN_ACCEL = 0.025;
+
+function filterAccel(val, prevVal) {
+  if (val < prevVal - NP_ACCEL || val > prevVal - NP_ACCEL) return val;
+  if (val < prevVal - NPS_ACCEL || val > prevVal - NPS_ACCEL)
+    return (val + prev_val) / 2;
+  return (1 - GAIN_ACCEL) * prevVal + GAIN_ACCEL * val;
+}
+
+const NP_MAG = 3.5;
+const GAIN_MAG = 0.025;
+
+function filterMag(val, prev_val) {
+    if (val < prevVal - NP_MAG || val > prevVal + NP_MAG) return val;
+    return (1 - GAIN_MAG) * prevVal + GAIN_MAG * val;
+}
+
+const NP_GYRO = 0.267;
+
+function filterGyro(val) {
+    if (abs(val) > NP_GYRO) return val;
+    return 0;
+}
