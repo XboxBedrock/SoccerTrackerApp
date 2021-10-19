d966b05232ab28f5746cce749b117138491f0ca7
XboxBedrock <68715625+XboxBedrock@users.noreply.github.com>
2021-10-18 20:23:17 -0700

    e

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 2021
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
