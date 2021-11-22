06720aacbaa8b5db35b266dcb4b657831710f415
PrajwalVandana <72575914+PrajwalVandana@users.noreply.github.com>
2021-11-21 20:12:20 -0800

    get locations from sensor readings

--- struct-unpack-tester.js
+++ struct-unpack-tester.js
@@ -1,58 +1,69 @@
 const struct = require("python-struct");
 const AHRS = require("ahrs")
 
-const buf = Buffer.from('abcd');
-console.log(struct.unpack('>'+'f'.repeat(buf.length/4), buf))
+const refreshRate = 20
 
-class Vector {
-    constructor(x, y, z) {
-        if (x instanceof Vector || typeof x === 'object') {
-            this.x = x.x
-            this.y = x.y
-            this.z = x.z
-        } else {
-            this.x = x
-            this.y = y
-            this.z = z
-        }
-    }
-
-    add(v) {
-        return new Vector(this.x + v.x, this.y + v.y, this.z + v.z)
-    }
-
-    subtract(v) {
-        return new Vector(this.x - v.x, this.y - v.y, this.z - v.z)
-    }
-
-    multiply(n) {
-        return new Vector(this.x * n, this.y * n, this.z * n)
-    }
-
-    divide(n) {
-        return new Vector(this.x / n, this.y / n, this.z / n)
-    }
-
-    fromQuaternion(q) {  // TODO: check if function is correct
-        return new Vector(q.y, q.z, q.w)
+function qmult(q1, q2) {
+    return {
+        w: q1.w * q2.w - q1.x * q2.x - q1.y * q2.y - q1.z * q2.z,
+        x: q1.w * q2.x + q1.x * q2.w + q1.y * q2.z - q1.z * q2.y,
+        y: q1.w * q2.y - q1.x * q2.z + q1.y * q2.w + q1.z * q2.x,
+        z: q1.w * q2.z + q1.x * q2.y - q1.y * q2.x + q1.z * q2.w
     }
 }
 
-function get_angles(arr) {
-    locations = new Array(arr.length)
-    locations[0] = [0, 0, 0]
+function qconj(q) {
+    return {
+        w: q.w,
+        x: -q.x,
+        y: -q.y,
+        z: -q.z
+    }
+}
+
+function rotateVector(vec, q) {
+    return qmult(qmult(q, { w: 0, x: vec.x, y: vec.y, z: vec.z }), qconj(q))
+}
+
+function addVectors(...vecs) {
+    res = { x: 0, y: 0, z: 0 }
+    for (let i = 0; i < vecs.length; i++) {
+        res.x += vecs[i].x
+        res.y += vecs[i].y
+        res.z += vecs[i].z
+    }
+}
+
+function getLocations(readings) {
+    locations = new Array(readings.length+1)
+    locations[0] = { x: 0, y: 0, z: 0 }
+
     const madgwick = new AHRS({
-        sampleInterval: 20,
+        sampleInterval: refreshRate,
         algorithm: "Madgwick",
         beta: 0.4,
-        doInitialisation: false
+        doInitialisation: true
     })
-    madgwick.init(arr[0])
-    for (let i = 1; i < arr.length; ++i) {
-        madgwick.update(...arr[i])
-        // locations[i+1] = locations[i] + Vector(madgwick.toVector())*arr.slice(0, 3)
-        console.log(madgwick.toVector())
+
+    velocity = { x: 0, y: 0, z: 0 }
+    for (let i = 0; i < readings.length; ++i) {
+        madgwick.update(...readings[i])
+        [ax_local, ay_local, az_local] = readings.slice(3, 6)
+        q = madgwick.getQuaternion()
+        acceleration = addVectors(
+            rotateVector({ x: ax_local, y: 0, z: 0 }, q),
+            rotateVector({ x: 0, y: ay_local, z: 0 }, q),
+            rotateVector({ x: 0, y: 0, z: az_local }, q)
+        )
+        velocity.x += acceleration.x / refreshRate
+        velocity.y += acceleration.y / refreshRate
+        velocity.z += acceleration.z / refreshRate
+        locations[i+1] = {
+            x: locations[i].x + velocity.x / refreshRate,
+            y: locations[i].y + velocity.y / refreshRate,
+            z: locations[i].z + velocity.z / refreshRate
+        }
     }
 }
 
-get_angles([[0, 1, 2, 3, 4, 5, 6, 7, 8]])
+getAngles([[0, 1, 2, 3, 4, 5, 6, 7, 8]])
